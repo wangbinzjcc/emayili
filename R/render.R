79374ee40d966c35041e29a60d208cc954dcf516
Andrew Collier <andrew@exegetic.biz>
2019-11-23 06:22:24 +0200

    Ideas from elsewhere.

--- R/render.R
+++ R/render.R
@@ -1,3 +1,51 @@
+# MD2HTML <- function (markdown, workingDirectory = NULL, environment = NULL) {
+#   SetKnitrOptions(workingDirectory)
+#   if (is.null(environment)) environment <- parent.frame()
+#   html <- knitr::knit2html(text=markdown, options=c("use_xhtml", "smartypants", "mathjax", "highlight_code"), stylesheet = GetMarkDownCss(), envir = environment)
+#
+#   return(html)
+# }
+#
+# RMD2HTML <- function (sourceFilePath, outputFilePath = NULL, environment = NULL) {
+#   markdown <- ReadFile(sourceFilePath)
+#   html <- MD2HTML(markdown, workingDirectory = sourceFilePath, environment)
+#
+#   # Strip "<!DOCTYPE html>" from top of HTML document (doesn't parse properly in mail).
+#   #
+#   html <- sub("^<!DOCTYPE html>\n", "", html)
+#
+#   if (!is.null(outputFilePath)) {
+#     WriteFile(text = html, fileName = outputFilePath)
+#   }
+#
+#   return(html)
+# }
+#
+# #Functions to encode images inline (base64)
+#
+# MD2HTML64 <- function (markdown, workingDirectory = NULL, environment = NULL) {
+#   SetKnitrOptions(workingDirectory)
+#   if (is.null(environment)) environment <- parent.frame()
+#   html <- knitr::knit2html(text=markdown, options=c("use_xhtml", "smartypants", "mathjax", "highlight_code", "base64_images"), stylesheet = GetMarkDownCss(), envir = environment)
+#
+#   return(html)
+# }
+#
+# RMD2HTML64 <- function (sourceFilePath, outputFilePath = NULL, environment = NULL) {
+#   markdown <- ReadFile(sourceFilePath)
+#   html <- MD2HTML64(markdown, workingDirectory = sourceFilePath, environment)
+#
+#   # Strip "<!DOCTYPE html>" from top of HTML document (doesn't parse properly in mail).
+#   #
+#   html <- sub("^<!DOCTYPE html>\n", "", html)
+#
+#   if (!is.null(outputFilePath)) {
+#     WriteFile(text = html, fileName = outputFilePath)
+#   }
+#
+#   return(html)
+# }
+
 #' Title
 #'
 #' @param msg A message object.
