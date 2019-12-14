b0af6ef619daffaa28d5726e1100a3f9a60d04c3
Andrew Collier <andrew@exegetic.biz>
2019-12-14 07:05:42 +0200

    Update render()

--- R/render.R
+++ R/render.R
@@ -1,52 +1,4 @@
-# MD2HTML <- function (markdown, workingDirectory = NULL, environment = NULL) {
-#   SetKnitrOptions(workingDirectory)
-#   if (is.null(environment)) environment <- parent.frame()
-#   html <- knitr::knit2html(text=markdown, options=c("use_xhtml", "smartypants", "mathjax", "highlight_code"), stylesheet = GetMarkDownCss(), envir = environment)
-#
-#   return(html)
-# }
-#
-# RMD2HTML <- function (sourceFilePath, outputFilePath = NULL, environment = NULL) {
-#   markdown <- ReadFile(sourceFilePath)
-#   html <- MD2HTML(markdown, workingDirectory = sourceFilePath, environment)
-#
-#   # Strip "<!DOCTYPE html>" from top of HTML document (doesn't parse properly in mail).
-#   #
-#   html <- sub("^<!DOCTYPE html>\n", "", html)
-#
-#   if (!is.null(outputFilePath)) {
-#     WriteFile(text = html, fileName = outputFilePath)
-#   }
-#
-#   return(html)
-# }
-#
-# #Functions to encode images inline (base64)
-#
-# MD2HTML64 <- function (markdown, workingDirectory = NULL, environment = NULL) {
-#   SetKnitrOptions(workingDirectory)
-#   if (is.null(environment)) environment <- parent.frame()
-#   html <- knitr::knit2html(text=markdown, options=c("use_xhtml", "smartypants", "mathjax", "highlight_code", "base64_images"), stylesheet = GetMarkDownCss(), envir = environment)
-#
-#   return(html)
-# }
-#
-# RMD2HTML64 <- function (sourceFilePath, outputFilePath = NULL, environment = NULL) {
-#   markdown <- ReadFile(sourceFilePath)
-#   html <- MD2HTML64(markdown, workingDirectory = sourceFilePath, environment)
-#
-#   # Strip "<!DOCTYPE html>" from top of HTML document (doesn't parse properly in mail).
-#   #
-#   html <- sub("^<!DOCTYPE html>\n", "", html)
-#
-#   if (!is.null(outputFilePath)) {
-#     WriteFile(text = html, fileName = outputFilePath)
-#   }
-#
-#   return(html)
-# }
-
-#' Title
+#' Render R Markdown document into email
 #'
 #' @param msg A message object.
 #' @param rmd_filepath Path to a Rmd file.
@@ -60,22 +12,18 @@ render <- function(msg, rmd_filepath, quiet = TRUE){
   output_path = tempfile(fileext = ".html")
   #
   rmarkdown::render(rmd_filepath, output_file = output_path, quiet = quiet)
-  #
-  # content <- knitr::knit2html(input=rmd_filepath, options=c("use_xhtml","smartypants","mathjax","highlight_code"))
-  # content = readLines(output_path) %>% paste(collapse = "\n")
-  # print(substr(content, 1, 500))
 
-  # content <- readChar(output_path, file.info(output_path)$size)
-  # print(content)
+  # OPTION 1
   #
-  # type <- "text/html"
-  # #
-  # body <- mime(type, "quoted-printable", NULL, "utf-8")
-  # body$body <- content
-  #
-  # msg$parts <- c(msg$parts, list(body))
+  # msg <- msg %>% attachment(output_path)
 
-  msg <- msg %>% attachment(output_path)
+  # OPTION 2
+  #
+  content <- readChar(output_path, file.info(output_path)$size)
+  content <- sub("^<!DOCTYPE html>\n", "", content)
+  msg <- msg %>% body(content, type = "html")
+
+  print(output_path)
 
   invisible(msg)
 }
